3f937cbe15d8d783f84eb8d974f4341a2c08c394
SheikhRF <sheikh.rf@oundleschool.org.uk>
2025-08-13 02:39:39 +0100

    show figure with demo of img and label

--- Dataset_test.py
+++ Dataset_test.py
@@ -18,8 +18,27 @@ test_data = datasets.FashionMNIST(
     transform=ToTensor()
 )
 
-print(training_data[0])
-img, label = training_data[0]
-plt.imshow(img.squeeze(), cmap="gray")
+labels_map = {
+    0: "T-shirt/top",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle boot"
+}
+
+figure = plt.figure(figsize=(8, 8))
+cols, rows = 3, 3
+for i in range(1, cols * rows + 1):
+    sample_idx = torch.randint(len(training_data), size=(1,)).item()
+    img, label = training_data[sample_idx]
+    figure.add_subplot(rows, cols, i)
+    plt.title(labels_map[label])
+    plt.axis("off")
+    plt.imshow(img.squeeze(), cmap="gray")
 plt.show()
-print(f"Label: {label}")
+
