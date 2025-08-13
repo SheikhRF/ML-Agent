eac1e59974f2f2ffa85faffa5b9ae0b8bc4acf42
SheikhRF <sheikh.rf@oundleschool.org.uk>
2025-08-13 18:36:02 +0100

    testing on untrained model and understanding

--- networkTesting.py
+++ networkTesting.py
@@ -4,6 +4,10 @@ from torch import nn
 from torch.utils.data import DataLoader
 from torchvision import datasets, transforms
 from torchvision.transforms import ToTensor
+import matplotlib.pyplot as plt
+
+device = torch.accelerator.current_accelerator().type if torch.accelerator .is_available() else "cpu"
+print(f"using {device} device")
 
 training_data = datasets.FashionMNIST(
     root="data",
@@ -22,8 +26,18 @@ test_data = datasets.FashionMNIST(
 train_dataloader = DataLoader(training_data, batch_size=64)
 test_dataloader = DataLoader(test_data, batch_size=64)
 
-device = torch.accelerator.current_accelerator().type if torch.accelerator .is_available() else "cpu"
-print(f"using {device} device")
+labels_map = {
+    0: "T-shirt/top",
+    1: "Trouser",
+    2: "Pullover",
+    3: "Dress",
+    4: "Coat",
+    5: "Sandal",
+    6: "Shirt",
+    7: "Sneaker",
+    8: "Bag",
+    9: "Ankle boot"
+}
 
 class NeuralNetwork(nn.Module):
     def __init__(self):
@@ -44,3 +58,18 @@ class NeuralNetwork(nn.Module):
 
 model = NeuralNetwork().to(device)
 
+
+for i in range(3):
+    sample_idx = torch.randint(len(test_data), size=(1,)).item()
+    img, label = test_data[sample_idx]
+    plt.imshow(img.squeeze(), cmap="gray")
+    plt.show()
+    logits = model(img.to(device))
+    pred_probab = nn.Softmax(dim=1)(logits)
+    print(logits)
+    print(pred_probab)
+    y_pred = pred_probab.argmax(1)
+    print(f"Predicted class: {y_pred}")
+    print(f"predicted class name: {labels_map[y_pred.item()]}")
+    print(label)
+    print(labels_map[label])
